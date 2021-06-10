8d0c836080aa746ee29c7191d575c2044f562172
elinln <linneaelinnilsson@gmail.com>
2021-06-10 11:39:53 +0200

    ändringar på filstruktur

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,22 +2,28 @@ import React from 'react';
 import '../index.css';
 import {Link} from 'react-router-dom';
 import ThemeOfChoice from './themes'
+import imageSource from './../assets/logo.png'
 
 function Nav() {
+
+  console.log(imageSource)
+
   return (
     <nav className="nav centerContent">
-      <h3>Logo</h3>
-      <ul className= "span">
-        <Link to='/gameboard'>
-          <li className= "span">Gameboard</li>
-        </Link>
-        <Link to='/info'>
-          <li className= "span">Info</li>
-        </Link>
-        <ThemeOfChoice />
-      </ul>
-    </nav>
-  );
+        <img src={imageSource} alt="" />
+        <ul className= "span">
+          <Link to='/gameboard'>
+            <li className= "span">Gameboard</li>
+          </Link>
+          <Link to='/info'>
+            <li className= "span">Info</li>
+          </Link>
+          <ThemeOfChoice />
+        </ul>
+      </nav>
+  )
 }
+
+
 
 export default Nav;
--- src/components/themes.tsx
+++ src/components/themes.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { CSSProperties } from 'react';
-import desert from './images/desert.png'
-import candyland from './images/candyland.png'
-import djungle from './images/djungle.png' 
- import mainpic from './images/2.jpg'
+import desert from './../assets/desert.png'
+import candyland from './../assets/candyland.png'
+import djungle from './../assets/djungle.png' 
+import mainpic from './../assets/2.jpg'
 import { useState } from 'react';
 import { url } from 'inspector';
 import { Select } from '@material-ui/core';
@@ -21,27 +21,27 @@ export default function ChooseTheme() {
         id: 1,
         label: "Default",
         value: "default",
-        backgroundImage: "./images/2.jpg"
+        backgroundImage: "./../assets/2.jpg"
     },
     {
         id: 2,
         label: "Candyland",
         value: "candyland",
-        backgroundImage: "./images/candyland.png"
+        backgroundImage: "./../assets/candyland.png"
 
     }, 
     {
         id: 3,
         label: "Desert",
         value: "desert",
-        backgroundImage: "./images/desert.png"
+        backgroundImage: "./../assets/desert.png"
 
     },
     {
         id: 4,
         label: "Djungle",
         value: "djungle",
-        backgroundImage: "./images/djungle.png"
+        backgroundImage: "./../assets/djungle.png"
 
     }]
 
