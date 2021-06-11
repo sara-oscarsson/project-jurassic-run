c1b3e395b7755f447eb3fa3d04e189790d1b11fe
elinln <linneaelinnilsson@gmail.com>
2021-06-11 12:00:51 +0200

    info-page och lite styling

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,8 +1,33 @@
-import * as React from 'react';
+import * as React from "react";
+import dinoPic from "./../assets/dino.png";
 
-export default function Info(){
-    return(
-        <h1>Info</h1>
-        
-    );
+export default function Info() {
+  return (
+    <div>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          fontSize: "45px",
+        }}
+      >
+        Vem är Dino?
+      </h1>
+      <div className="infoDino">
+        <img
+          src={dinoPic}
+          alt=""
+          style={{
+            width: "45%",
+          }}
+        />
+        <h3>
+          Dino är en väldigt snäll dinosaurie egentligen. Han är en T-Rex som
+          älskar köttbullar med potatismos, men ogillar att stöta på hinder.
+          Hjälp Dino att hoppa över alla hinder i spelet för att Dino inte ska
+          bli ledsen!
+        </h3>
+      </div>
+    </div>
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,52 +1,51 @@
-import React, { useState } from 'react';
-import '../index.css';
-import {Link} from 'react-router-dom';
-import imageSource from './../assets/logo.png'
-import ThemeOfChoice, { Theme } from './themes';
-import ChooseTheme from './themes';
-import SimpleModal from './rulesModal';
-import ExtraModal from './extraModal';
+import React, { useState } from "react";
+import "../index.css";
+import { Link } from "react-router-dom";
+import imageSource from "./../assets/logo.png";
+import ThemeOfChoice, { Theme } from "./themes";
+import ChooseTheme from "./themes";
+import SimpleModal from "./rulesModal";
+import ExtraModal from "./extraModal";
 
 interface Props {
-  setBackground: (theme: Theme) => void,
+  setBackground: (theme: Theme) => void;
 }
 
 interface ExtraProps {
-  isOpen: boolean
+  isOpen: boolean;
 }
-
 
 function Nav(props: Props, extraProps: ExtraProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const changeModalState = ()=>{
-    setIsModalOpen(true)
-    
-  }
+  const changeModalState = () => {
+    setIsModalOpen(true);
+  };
   return (
     <nav className="nav centerContent">
-      <img src={imageSource} alt="" />
-      <ul className= "span">
-        <Link to='/gameboard'>
-          <li className= "span">Gameboard</li>
+      <Link to="/">
+        <img src={imageSource} alt="" />
+      </Link>
+      <ul className="span">
+        <Link to="/gameboard">
+          <li className="span">Gameboard</li>
         </Link>
-        <Link to='/info'>
-          <li className= "span">Info</li>
+        <Link to="/info">
+          <li className="span">Info</li>
         </Link>
-        <ChooseTheme setBackground = {props.setBackground}/>
+        <ChooseTheme setBackground={props.setBackground} />
       </ul>
-      <SimpleModal/>
+      <SimpleModal />
       <button onClick={changeModalState}>Modal med react router</button>
-      {
-          isModalOpen ? (
-            <ExtraModal isOpen={isModalOpen}>
-            <h3>
-            Här är en modal 
-            <button className="btn-close" onClick={() => setIsModalOpen(false)}>Stäng</button>
-            </h3>
-            </ExtraModal>
-
-          ) : null
-        }
+      {isModalOpen ? (
+        <ExtraModal isOpen={isModalOpen}>
+          <h3>
+            Här är en modal
+            <button className="btn-close" onClick={() => setIsModalOpen(false)}>
+              Stäng
+            </button>
+          </h3>
+        </ExtraModal>
+      ) : null}
 
       {/* <div>
         <button onClick={() => setIsOpen(true)}>Modalicuious</button>
@@ -55,7 +54,5 @@ function Nav(props: Props, extraProps: ExtraProps) {
     </nav>
   );
 }
-
-
 
 export default Nav;
