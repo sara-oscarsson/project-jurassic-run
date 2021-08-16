017a3a3d4a3e2ee9c3d76191ab031cc7473bbea4
Tim-bitz <timbenjaminson@hotmail.com>
2021-08-16 10:09:24 +0200

    Ljudkontroll, dold, ljud funkar ej

--- src/components/mediaComponent.tsx
+++ src/components/mediaComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Script } from 'vm'
 
-const themeSong = require('../assets/themeSong.mp3')
+require('./../assets/themeSong.mp3')
 
 export const MediaComponent = () => {
 
@@ -8,14 +9,8 @@ export const MediaComponent = () => {
 
     return (
         <>
-
-        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/zHalXjs0cDA" title="YouTube player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;" allowFullScreen></iframe> */}
-            <div>
-                Tryck in content här.
-            </div>    
-            <audio autoPlay loop controls>
-                <source src= {themeSong} type="audio/mp3"/>
-                Your browser does not support the audio element.
+            <audio>
+                <source src= './../assets/themeSong.mp3' type="audio/mp3"></source>
             </audio>
         </>
     )
